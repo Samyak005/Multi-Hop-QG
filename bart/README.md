f5169d36aa5b0e57f62267cbfa25fdd65cca2d61
Nirmal Manoj <nimmumanoj@gmail.com>
2021-11-22 01:28:44 +0530

    BART code with evaluation results on dev dataset (#1)

---
+++ bart/README.md
@@ -0,0 +1,12 @@
+### File structure
+1. `bart_supporting_facts_training.py` - python training script.
+3. `single_inference.ipynb` - testing script on a single example.
+4. `testing_inference.ipynb` - testing script on full dataset.
+5. `predicted_full_context.csv`- Predictions CSV file for full context setting.
+6. `predicted_supporting_facts.csv` - Predictions CSV file for supporting facts setting.
+
+### Libraries required
+1. Pytorch
+2. transformers
+3. numpy
+4. pandas
---
+++ evaluation/evaluation.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from nlgeval import NLGEval
+from collections import OrderedDict
+import json
+
+if __name__ == '__main__':
+    # df = pd.read_csv('predicted_supporting_facts1.csv') # T5 Supporting Facts
+    # df = pd.read_csv('predicted_full_context.csv') # T5 Full Context
+    # df = pd.read_csv('predicted_gpt2_context.csv') # GPT2 Full context (first 2)
+    # df = pd.read_csv('../bart/predicted_supporting_facts.csv') # BART Supporting Facts
+    df = pd.read_csv('../bart/predicted_full_context.csv') # BART Full Context
+    ref = df["question"].tolist()
+    hyp = df["predicted"].tolist()
+
+    n = NLGEval(metrics_to_omit=["CIDEr", "SkipThoughtCS",
+                "EmbeddingAverageCosineSimilarity", "VectorExtremaCosineSimilarity", "GreedyMatchingScore"])
+
+    scores = n.compute_metrics(ref_list=[ref], hyp_list=hyp)
+
+    scores['Bleu_mean_eq_weight'] = (scores['Bleu_1'] + \
+        scores['Bleu_2'] + scores['Bleu_3'] + scores['Bleu_4']) / 4.0
+
+    scores = dict(OrderedDict(sorted(scores.items())))
+
+    print(scores)
+    with open('evaluation_results.json', 'w') as fp:
+        json.dump(scores, fp, indent=4)
